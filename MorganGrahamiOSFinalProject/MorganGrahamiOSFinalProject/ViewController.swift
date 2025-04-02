dc8966dedba384a9bda851a895fa346cc5b55a42
Owen Graham <omgraham02@gmail.com>
2025-04-02 17:36:44 -0500

    Added first view connections

--- MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProject/ViewController.swift
+++ MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProject/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputOL: UITextField!
+    @IBOutlet weak var convertFromPV: UIPickerView!
+    @IBOutlet weak var convertToPV: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
