1c71ba99769752227de66cb946c60ff5cb0390b7
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-02 17:07:56 -0500

    created repo and final project XCode document

---
+++ MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProjectTests/MorganGrahamiOSFinalProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorganGrahamiOSFinalProjectTests.swift
+//  MorganGrahamiOSFinalProjectTests
+//
+//  Created by Wyatt Morgan on 4/2/25.
+//
+
+import Testing
+@testable import MorganGrahamiOSFinalProject
+
+struct MorganGrahamiOSFinalProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
