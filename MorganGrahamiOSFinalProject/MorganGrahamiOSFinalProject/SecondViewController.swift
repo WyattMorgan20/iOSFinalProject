962a3b8ba0079ef5d2c7635aa3b8d995423420b9
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-15 11:29:09 -0500

    pickerview general code added

--- MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProject/SecondViewController.swift
+++ MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProject/SecondViewController.swift
@@ -8,22 +8,69 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    @IBOutlet weak var startingMeasurmentInputOL: UITextField!
     
     @IBOutlet weak var startingMeasurmentDisplayOL: UILabel!
     
     @IBOutlet weak var convertedMeasurementOutputOL: UILabel!
     
+    var current = 0.0
+    var converted = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func convertBTNPressed(_ sender: UIButton) {
+        
+        
         
     }
+    
+    /*
+     demo pickerview code:
+     var fromPicker: [String] = [String]()
+     var toPicker: [String] = [String]()
+     
+     override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        // Connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        // Input the data into the arrays
+        fromPicker = ["Miles", "Kilometers", "Feet", "Inches", "Centimeters"]
+        
+        toPicker = ["Miles", "Kilometers", "Feet", "Inches", "Centimeters"]
+     
+     override func didReceiveMemoryWarning() {
+         super.didReceiveMemoryWarning()
+         // Dispose of any resources that can be recreated.
+     }
+
+     // Number of columns of data
+     func numberOfComponents(in pickerView: UIPickerView) -&gt; Int {
+         return 1
+     }
+     
+     // The number of rows of data
+     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -&gt; Int {
+         return fromPicker.count, toPicker.count
+     }
+     
+     // The data to return fopr the row and component (column) that's being passed in
+     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -&gt; String? {
+         return fromPicker[row], toPicker[row]
+     }
+     
+     // Capture the picker view selection
+     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+         // This method is triggered whenever the user makes a change to the picker selection.
+         // The parameter named row and component represents what was selected.
+     }
+ ...
+     }
+     */
+
     
     /*
     // MARK: - Navigation
