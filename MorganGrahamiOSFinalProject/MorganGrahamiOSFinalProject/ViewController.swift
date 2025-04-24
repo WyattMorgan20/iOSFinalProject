903689080c79b0e1cd9419702f586c93c810f572
Wyatt Morgan <iamtheyy@gmail.com>
2025-04-24 11:29:27 -0500

    fixed conversion logic issue

--- MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProject/ViewController.swift
+++ MorganGrahamiOSFinalProject/MorganGrahamiOSFinalProject/ViewController.swift
@@ -95,122 +95,40 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBAction func placeHolderBTN(_ sender: UIButton) {
         current = Double(inputOL.text!)!
         
-        if valueSelectedFrom == "Miles" {
-            Miles = current
-            Kilometers = 1.6
-            Feet = 5280
-            Inches = 63360
-            Centimeters = 160934.4
-            
-            if valueSelectedTo == "Kilometers" {
-                converted = Miles * Kilometers
-            }
-            else if valueSelectedTo == "Feet" {
-                converted = Miles * Feet
-            }
-            else if valueSelectedTo == "Inches" {
-                converted = Miles * Inches
-            }
-            else if valueSelectedTo == "Centimeters" {
-                converted = Miles * Centimeters
-            }
+        // Converting all values for input to meters
+        var inMeters = 0.0
+        switch valueSelectedFrom {
+        case "Miles":
+            inMeters = current * 1609.344
+        case "Kilometers":
+            inMeters = current * 1000
+        case "Feet":
+            inMeters = current * 0.3048
+        case "Inches":
+            inMeters = current * 0.0254
+        case "Centimeters":
+            inMeters = current * 0.01
+        default:
+            inMeters = current
         }
-        else if valueSelectedFrom == "Kilometers" {
-            Kilometers = current
-            Miles = 0.6
-            Feet = 3280.8
-            Inches = 40467.2
-            Centimeters = 100000
             
-            if valueSelectedTo == "Miles" {
-                converted = Kilometers * Miles
-            }
-            else if valueSelectedTo == "Feet" {
-                converted = Kilometers * Feet
-            }
-            else if valueSelectedTo == "Inches" {
-                converted = Kilometers * Inches
-            }
-            else if valueSelectedTo == "Centimeters" {
-                converted = Kilometers * Centimeters
-            }
+        // Converting meters to target unit
+        switch valueSelectedTo {
+        case "Miles":
+            converted = inMeters / 1609.344
+        case "Kilometers":
+            converted = inMeters / 1000
+        case "Feet":
+            converted = inMeters / 0.3048
+        case "Inches":
+            converted = inMeters / 0.0254
+        case "Centimeters":
+            converted = inMeters / 0.01
+        default:
+            converted = inMeters
         }
-        else if valueSelectedFrom == "Feet" {
-            Feet = current
-            Kilometers = 0.000189394
-            Miles = 0.000000568182
-            Inches = 12
-            Centimeters = 30.48
-            
-            if valueSelectedTo == "Miles" {
-                converted = Feet * Miles
-            }
-            else if valueSelectedTo == "Kilometers" {
-                converted = Feet * Kilometers
-            }
-            else if valueSelectedTo == "Inches" {
-                converted = Feet * Inches
-            }
-            else if valueSelectedTo == "Centimeters" {
-                converted = Feet * Centimeters
-            }
-        }
-        else if valueSelectedFrom == "Inches" {
-            Inches = current
-            Kilometers = 0.0000254
-            Miles = 0.0000000833333
-            Feet = 0.0833333
-            Centimeters = 2.54
-            
-            if valueSelectedTo == "Miles" {
-                converted = Inches * Miles
-            }
-            else if valueSelectedTo == "Kilometers" {
-                converted = Inches * Kilometers
-            }
-            else if valueSelectedTo == "Feet" {
-                converted = Inches * Feet
-            }
-            else if valueSelectedTo == "Centimeters" {
-                converted = Inches * Centimeters
-            }
-        }
-        else if valueSelectedFrom == "Centimeters" {
-            Centimeters = current
-            Kilometers = 0.000001
-            Miles = 0.000000000621371
-            Feet = 0.00328084
-            Inches = 0.393701
-            
-            if valueSelectedTo == "Miles" {
-                converted = Centimeters * Miles
-            }
-            else if valueSelectedTo == "Kilometers" {
-                converted = Centimeters * Kilometers
-            }
-            else if valueSelectedTo == "Feet" {
-                converted = Centimeters * Feet
-            }
-            else if valueSelectedTo == "Inches" {
-                converted = Centimeters * Inches
-            }
-            
-            self.performSegue(withIdentifier: "secondVCSegue", sender: self)
-        }
-        
-//        ✅ Fix:
-//        Option 1: Manually trigger the segue after the values are updated
-//
-//        If your segue is set in storyboard (via button dragging to next view controller), change it to manual and trigger it in code like this:
-//
-//        Remove the storyboard segue from the button.
-//        Assign it an identifier, like "secondVCSegue"
-//        Add this to your button action, at the end of placeHolderBTN:
-//        self.performSegue(withIdentifier: "secondVCSegue", sender: self)
-//        This ensures the segue only happens after current and converted have been updated.
         
         self.performSegue(withIdentifier: "secondVCSegue", sender: self)
-        
         
     }
     
